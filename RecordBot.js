70afb4b7da093e6b598469341f564ba63e513d48
nBow <nbow51@gmail.com>
2019-01-08 17:38:43 -0800

    working bot?

--- RecordBot.js
+++ RecordBot.js
@@ -1,6 +1,7 @@
 var Discord = require("discord.io");
 var logger = require("winston");
 var auth = require("./auth.json");
+var fs = require("fs");
 
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -32,6 +33,36 @@ bot.on("message", function(user, userID, channelID, message, evt) {
                 to: channelID,
                 message: "Pong!"
             });
+            case "csv":
+            // Use this code for each role
+            function csvRole(nameSTRING, idSTRING){
+                let roleName = nameSTRING
+                let thisRole = message.guild.roles.get(idSTRING).members.map(m=>m.user.tag);
+                thisRole.join(` ${roleName},`);
+                thisRole.split(" ");
+                thisRole.unshift(`${roleName},`);
+                thisRole.join("\n")
+            }
+
+            let content = [
+                // csvRole("Admin", "1231354425632433"),
+                // csvRole("HR", "1231231241241241")
+            ].join("\n");
+
+            fs.writeFile("/logs/ranks.csv", content, function(err){
+                if (err) throw err;
+                bot.sendMessage({
+                    to: channelID,
+                    message: "CSV created."
+                })
+            })
+            message.channel.send(new Discord.Attachment("./logs/ranks.csv", "ranks.csv"))
+            .then(msg => {
+                bot.sendMessage({
+                    to: channelID,
+                    message: "CSV added to files."
+                })
+            }).catch(console.error);
             break;
         }
     }
