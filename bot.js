0b0afb33483193111143ae37f1aafbea28820445
nBow <nbow51@gmail.com>
2019-01-12 01:29:38 -0800

    updated bot to work with any server

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const mkdirp = require('mkdirp');
 const { Client, Attachment } = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config()
@@ -8,11 +9,6 @@ const key = process.env;
 
 
 client.on("ready", () => {
-    var dir = __dirname + '/logs';
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
     console.log("I am ready!");
 })
 
@@ -30,46 +26,58 @@ client.on("message", message => {
             case "job":
                 message.channel.send("My job is to record our servers ranks and the names of those who belongs to those ranks!");
                 break;
-            case "test":
-                console.log(message.guild.roles.map(role => role.id))
-                console.log(message.guild.roles.map(role => role.name))
-                break;
+            // case "test":
+            //     console.log(message.guild.roles.map(role => role.id))
+            //     console.log(message.guild.roles.map(role => role.name))
+            //     console.log(message.guild.name);
+            //     mkdirp(`${__dirname}/logs/${message.guild.name}/${message.channel.name}`, function(err){
+            //         if (err) console.error(err)
+            //         else console.log("created dir :)")
+            //     })
+            //     break;
             case "csv":
-                function runCSV() {
-                    //Tags come in based on what role id you entered for args, after it get tags it maps it into an array
-                    var showContent = [];
-                    var roleIDArr = message.guild.roles.map(role => role.id).slice(1);
-                    var roleNamesArr = message.guild.roles.map(role => role.name).slice(1);
-
-                    for (var l = 0; l < roleIDArr.length; l++) {
-                        csvRole(roleIDArr[l], roleNamesArr[l]);
-                        console.log(roleIDArr[l] + ": " + roleNamesArr[l]);
-                    }
-                    setTimeout(function () {
-                        var finalContent = showContent.join("");
-                        fs.writeFile(`${__dirname}/logs/ranks.csv`, finalContent, function (err) {
-                            if (err) throw err;
-                        })
-                    }, 2000)
-                    setTimeout(function () {
-                        var attachment = message.channel.send(new Attachment(`${__dirname}/logs/ranks.csv`))
-                        message.channel.send("CSV File created!", attachment)
-                    }, 3500)
-
-                    function csvRole(roleID, roleName) {
-                        let thisRole = message.guild.roles.get(roleID).members.map(m => m.user.tag);
-                        for (let i = 0; i < thisRole.length; i++) {
-                            showContent.push(`${roleName},${thisRole[i]}\n`);
-                        }
-
-                    }
-                }
+                mkdirp(`${__dirname}/logs/${message.guild.name}/${message.channel.name}`, function(err){
+                    if (err) console.error(err)
+                    else console.log("created dir :)")
+                })
                 runCSV();
                 break;
         }
     }
+    function runCSV() {
+        //Tags come in based on what role id you entered for args, after it get tags it maps it into an array
+        var showContent = [];
+        var guild = message.guild.name;
+        var channel = message.channel.name;
+        var roleIDArr = message.guild.roles.map(role => role.id).slice(1);
+        var roleNamesArr = message.guild.roles.map(role => role.name).slice(1);
+        for (var l = 0; l < roleIDArr.length; l++) {
+            csvRole(roleIDArr[l], roleNamesArr[l]);
+            console.log(roleIDArr[l] + ": " + roleNamesArr[l]);
+        }
+        setTimeout(function () {
+            var finalContent = showContent.join("");
+            fs.writeFile(`${__dirname}/logs/${guild}/${channel}/ranks.csv`, finalContent, function (err) {
+                if (err) throw err;
+            })
+        }, 2000)
+        setTimeout(function () {
+            var attachment = message.channel.send(new Attachment(`${__dirname}/logs/${guild}/${channel}/ranks.csv`))
+            message.channel.send("CSV File created!", attachment)
+        }, 3500)
+    
+        function csvRole(roleID, roleName) {
+            let thisRole = message.guild.roles.get(roleID).members.map(m => m.user.tag);
+            for (let i = 0; i < thisRole.length; i++) {
+                showContent.push(`${roleName},${thisRole[i]}\n`);
+            }
+    
+        }
+    }
 
 })
+
+
 
 client.on('error', console.error)
 
